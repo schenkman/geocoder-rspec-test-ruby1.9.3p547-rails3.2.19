b6a662cc72f31a0715f24c48a1124daf1e064444
Chris Schenk <chris.schenk@gmail.com>
2014-11-29 18:28:29 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeocoderRspecTestRuby193p547Rails3219::Application.config.secret_token = 'c89d89dd5c23d1c169e6869b41295aee3ca7c80ba76b8415a249f38d7d807917b4d6d0d3c31642a89f21dcec1ca94bbd7dd5073fc9d7b2db34db712bed20689e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GeocoderRspecTestRuby193p547Rails3219::Application.config.session_store :cookie_store, key: '_geocoder-rspec-test-ruby1.9.3p547-rails3.2.19_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GeocoderRspecTestRuby193p547Rails3219::Application.config.session_store :active_record_store
